10d58df8c2c7e94776631584423bc70c7d0fe3c2
Paul Chheang <phalcheat@gmail.com>
2019-11-21 18:09:19 -0600

    Fully functional version, but need to update documentation in readme.md

--- assets/script/script.js
+++ assets/script/script.js
@@ -81,6 +81,44 @@ for (let i = 0; i < hourEls.length; i++) {
 // *** RETRIEVE SAVED DATA
 // - Check if array exists, if not then create empty array:
 // - Compary each object in the array if the key saved (date + time) === current date + time of each event hour
+const saveBtnEls = document.querySelectorAll(".saveBtn");
+const eventTextAreaEls = document.querySelectorAll(".event-js");
+const array1 = []; // [{hour: '10AM', eventTextEl: "eventTextAreaEl", buttonEl: "btnObj" }
+
+for (let i = 0; i < saveBtnEls.length; i++) {
+    array1.push({ "hour": hourEls[i].innerHTML, "eventTextEl": eventTextAreaEls[i], "button": saveBtnEls[i] });
+}
+console.log(array1);
+// add click to each save button
+for (let i = 0; i < array1.length; i++) {
+    console.log(array1[i].button);
+    array1[i].button.addEventListener("click", function () {
+        if (array1[i].eventTextEl.value === "") {
+            alert("nothing");
+        } else {
+            // save to localstorage
+            const dataStr = localStorage.getItem("event") || "[]";
+            const eventData = JSON.parse(dataStr);
+            eventData.push({ date: currentDay, time: array1[i].hour, content: array1[i].eventTextEl.value });
+            localStorage.setItem("event", JSON.stringify(eventData));
+            // console.log(eventData);
+        }
+    });
+}
+
+// Output the saved event onto the calender
+const dataStr = localStorage.getItem("event") || "[]";
+const data = JSON.parse(dataStr);
+console.log("data: ", data);
+
+    for (let i = 0; i < hourEls.length; i++) {
+        for (j = 0; j < data.length; j++) {
+            if (data[j].time === hourEls[i].innerHTML && data[j].date === currentDay) {
+                eventTextAreaEls[i].value = data[j].content;
+            }
+        }
+    }
+
 
 
 
